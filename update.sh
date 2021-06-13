4cd4f360b0af23ea8d1f44ace90ccea0726cd45a
Carmelo meteor <83161735+carmelo-meteor@users.noreply.github.com>
2021-06-13 12:36:19 +0200

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,6 +1,21 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
+set -e
 cd /home/pi/carmelo_meteor
-git pull origin main
-cp *.py ../
-sudo reboot
+
+local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+remote=$(git config branch.$local_branch.remote)
+
+echo "Fetching from $remote..."
+git fetch $remote
+
+if git merge-base --is-ancestor $remote_branch HEAD; then
+    echo 'Already up-to-date'
+    exit 0
+fi
+
+if git merge-base --is-ancestor HEAD $remote_branch; then
+    echo 'Fast-forward possible. Merging...'
+    git merge --ff-only --stat $remote_branch
+    sudo reboot
+fi
