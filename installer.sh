fdb8bf19db3279efc14a572ec3e55b06f5b04203
Carmelo meteor <83161735+carmelo-meteor@users.noreply.github.com>
2021-06-13 20:36:20 +0200

    Repository update at 13-06-2021

--- installer.sh
+++ installer.sh
@@ -92,7 +92,6 @@ echo "Restart=always" | sudo tee -a  /etc/systemd/system/carmelo.service > /dev/
 echo " " | sudo tee -a  /etc/systemd/system/carmelo.service > /dev/null
 echo "[Install]" | sudo tee -a  /etc/systemd/system/carmelo.service > /dev/null
 echo "WantedBy=multi-user.target" | sudo tee -a  /etc/systemd/system/carmelo.service > /dev/null
-echo
 
 ### 2. spedisci.service
 
@@ -106,7 +105,6 @@ echo "ExecStart=/usr/bin/python3 /home/pi/spedisci.py" | sudo tee -a  /etc/syste
 echo " " | sudo tee -a  /etc/systemd/system/spedisci.service > /dev/null
 echo "[Install]" | sudo tee -a  /etc/systemd/system/spedisci.service > /dev/null
 echo "WantedBy=multi-user.target" | sudo tee -a  /etc/systemd/system/spedisci.service > /dev/null
-echo
 
 ### 3. spedisci.timer
 
@@ -118,8 +116,6 @@ echo "OnCalendar=*:0/5" | sudo tee -a  /etc/systemd/system/spedisci.timer > /dev
 echo " " | sudo tee -a  /etc/systemd/system/spedisci.timer > /dev/null
 echo "[Install]" | sudo tee -a  /etc/systemd/system/spedisci.timer > /dev/null
 echo "WantedBy=timers.target" | sudo tee -a  /etc/systemd/system/spedisci.timer > /dev/null
-echo
-
 
 ### 4. update.service
 echo "[Unit]" | sudo tee -a  /etc/systemd/system/update.service > /dev/null
@@ -132,7 +128,6 @@ echo "ExecStart=/home/pi/update.sh" | sudo tee -a  /etc/systemd/system/update.se
 echo " " | sudo tee -a  /etc/systemd/system/update.service > /dev/null
 echo "[Install]" | sudo tee -a  /etc/systemd/system/update.service > /dev/null
 echo "WantedBy=multi-user.target" | sudo tee -a  /etc/systemd/system/update.service > /dev/null
-echo
 
 ### 5. update.timer
 
@@ -144,7 +139,6 @@ echo "OnCalendar=*-*-* 19:02:30" | sudo tee -a  /etc/systemd/system/update.timer
 echo " " | sudo tee -a  /etc/systemd/system/update.timer > /dev/null
 echo "[Install]" | sudo tee -a  /etc/systemd/system/update.timer > /dev/null
 echo "WantedBy=timers.target" | sudo tee -a  /etc/systemd/system/update.timer > /dev/null
-echo
 
 ### 6. receiving_station_data.txt
 
@@ -188,8 +182,6 @@ while :; do
 done
 
 
-
-
 while :; do
     echo -n "Inserisci l’angolo di vista della antenna in gradi es.: 360 oppure meno se ci sono ostacoli: "
     read VIEW
@@ -198,7 +190,6 @@ while :; do
     echo "$VIEW" | sudo tee -a  /home/pi/receiving_station_data.txt > /dev/null
     break  
 done
-
 
 while :; do
     echo -n "Inserisci il simbolo con il quale si vuole comparire nella rappresentazione complessiva di Carmelo scegliendo tra: asterisk circle circle_cross circle_dot circle_x circle_y cross dash diamond diamond_cross diamond_dot dot hex hex_dot inverted_triangle plus square square_cross square_dot square_pin square_x star star_dot triangle triangle_dot triangle_pin x y : "
