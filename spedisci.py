8ca1e4386eb00799bd136b6e2701cee587a57e8f
Carmelo meteor <83161735+carmelo-meteor@users.noreply.github.com>
2022-01-24 19:24:00 +0100

    Repository update at 24-01-2022

--- spedisci.py
+++ spedisci.py
@@ -1,34 +1,34 @@
-#versione del 16-06-2021 ore 19.39
+#versione del 24-01-2022 ore 19.22
 
 from pathlib import Path
 import os
 from paho.mqtt.client import Client
 import time
 
+#versione 3
+
 keyword = "ID"
-path = os.chdir("/tmp")#.dirname(os.path.realpath(__file__)) #imposta la directory in cui lavora
+path = os.chdir("tmp")#.dirname(os.path.realpath(__file__)) #imposta la directory in cui lavora
 
 elenco = [f for f in os.listdir(path) if f.endswith('.log')]
 
-
-client = Client(client_id="client_1")
-
+client = Client()
 
 client.connect('3.22.58.103', 1883)
 
 for file in elenco:
 
-    if keyword in file:
-        genere = "sono_vivo"
-    else:
-        genere = "meteora"
+        if keyword in file:
+            genere = "sono_vivo"
+        else:
+            genere = "meteora"
 
+        with open(file) as inputfile:
+            nome_file = str(file)
+            testo_file = inputfile.read()
+            testo_messaggio = nome_file + testo_file
+            client.publish(topic=genere, payload=testo_messaggio)
 
-    with open(file) as inputfile:
-        nome_file = str(file)
-        testo_file = inputfile.read()
-        testo_messaggio = nome_file + testo_file
+        os.unlink(Path(file))
 
-        client.publish(topic=genere, payload=testo_messaggio)
-
-    os.unlink(Path(file))
+        print(nome_file, " sent")
