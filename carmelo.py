0a8030ec58f7a901b5ec677d107ca72b56049fae
Carmelo meteor <83161735+carmelo-meteor@users.noreply.github.com>
2024-04-01 09:53:54 +0200

    Repository update at 01-04-2024

--- carmelo.py
+++ carmelo.py
@@ -1,9 +1,9 @@
 # CARMELO (Cheap Amatorial Radio MEteor Logger)
 # di Lorenzo Barbieri e Gaetano Brando
 
-vers="2_15"
+vers="2_16"
 from gpiozero import LED
-###---------------------------------accende i led per mostrare che sta caricando
+###------------------------------------------------------------------------------accende i led per mostrare che sta caricando
 ledverde=LED(17)
 ledgiallo=LED(6)
 ledrosso=LED(5)
@@ -23,7 +23,7 @@ sdr = RtlSdr()
 sleep (1)
 ledgiallo.off()
 
-###----------------------------------------legge e carica iparametri di stazione
+###------------------------------------------------------------------------------legge e carica iparametri di stazione
 stazione = [line.rstrip('\n') for line in open('/home/pi/receiving_station_data.txt')]
 localita = stazione[0]
 lat = float(stazione[1])
@@ -33,25 +33,28 @@ Tx = float(stazione[4])
 vista=float(stazione[5])
 segno=stazione[6]
 colore=stazione[7]
-soglia = 0.05  #0.1  ----------------------------soglia sul rumore per il trigger "meteora"
+soglia = 0.05  #0.1  ------------------------------------------------------------soglia sul rumore per il trigger "meteora"
 ###-----------------------------------------------------------------------------
 sleep (1)
 ledverde.off()
-###-----------------------------------------------------------caricamento finito
+###------------------------------------------------------------------------------caricamento finito
 camp = 8192     #2048
 shift = 0.1e6
 rxmedio = 50
-finestra = 0.067  #0.0015     0.067
-finestrina= 0.001
+##finestra = 0.067  #0.0015     0.067
+##finestrina= 0.001
+finestra = Tx/(10e9)    #14  KHz (per Graves)
+finestrina= Tx/(100e9)  #1.4 KHz
+
 cont =  rxm = trig = inizio = cok = 0
 contatore =0
-contmax = 500   ##---------------------------------------------------------------numero conteggi per stabilire la soglia
-trigmax=35      ##--------------50-------------------------------------------------attesa dopo la meteora prima di chiudere
+contmax = 200   ##---------------------------------------------------------------numero conteggi per stabilire la soglia
+trigmax=35      ##--------------50-----------------------------------------------tesa dopo la meteora prima di chiudere
 
 sdr.center_freq = Tx-shift
-sdr.sample_rate = 1.2e6  # 1.2e6-----------------------------------------------------frequenza di campionamento in Hz!!!!!
-sdr.freq_correction = 1   #  1 --------------------------------------------------- PPM
-sdr.gain = 15  ##---5
+sdr.sample_rate = 1.2e6  # 1.2e6-------------------------------------------------frequenza di campionamento in Hz!!!!!
+sdr.freq_correction = 1   #  1 --------------------------------------------------PPM
+sdr.gain = 15   ##---5
 pre_gain = 18.7 ##----preampl SPF5189 LNA
 px=0
 rumore=0
@@ -61,10 +64,10 @@ ggg=0
 def get_data():
     global rx,frequenza
     frame = sdr.read_samples(camp)  #--------------------------------------------acquisisce lo spettro
-    freq,power=signal.periodogram(frame, fs=1.0, window='boxcar')#----------------------------effettua l'FFT
+    freq,power=signal.periodogram(frame, fs=1.0, window='boxcar')#---------------effettua l'FFT
     freq = freq+0.016 + sdr.center_freq/1e6
     rx = frequenza=0
-    for i in range(0,len(frame)):  #---------------------------------------------------porzione di spettro
+    for i in range(0,len(frame)):  #---------------------------------------------porzione di spettro
         if power[i]>rx:
             rx=power[i]
             frequenza=freq[i]
@@ -87,7 +90,7 @@ while True:
             rxmedio=rxm/contmax
             rumore= (10*np.log10(rxmedio))-sdr.gain - pre_gain
             cont=rxm=0
-            #-------------------------------------------------------manda il messaggio "sono vivo"
+            #--------------------------------------------------------------------manda il messaggio "sono vivo"
             d = datetime.date.today()
             if ggg != d.day:
                 ledrosso.on()
@@ -105,7 +108,7 @@ while True:
                     sleep (1)
                     ledrosso.off()
 
-            #--------------------------------------------------------
+            #--------------------------------------------------------------------
 
     if rx > rxmedio + (rxmedio*soglia) and (Tx/1e6 - finestrina) < frequenza < (Tx/1e6 + finestra): #-------------inizio meteora
         trig=trigmax
@@ -141,8 +144,8 @@ while True:
     if trig==1:  #---------------------------------------------------------------fine rilevazione
         ledgiallo.off()
         fp=(100*cok/contatore)-20  # valutazione perc. sul falso positivo e densità
-##        if contatore>trigmax and round (secondaf,2)==Tx/1e6 and fp>-13: #------------------se è consistente e con due freq==tx allora stampa
-        if contatore>trigmax and (Tx/1e6 - finestrina) < secondaf < (Tx/1e6 + finestra) and fp>-13:
+        if contatore>trigmax and round (secondaf,2)==Tx/1e6 and fp>-13: #--------se è consistente e con due freq==tx allora stampa
+##        if contatore>trigmax and (Tx/1e6 - finestrina) < secondaf < (Tx/1e6 + finestra) and fp>-13:
             ledrosso.on()
             pippo=np.amax(meteora,axis=0)
             pot_max=round(pippo[3],2)
